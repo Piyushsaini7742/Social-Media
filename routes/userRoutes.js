a440715ea7ed85eef9bb7c144a22b156e31305be
Piyush <piyushsainiyt868@gmail.com>
2025-05-08 11:47:23 +0530

    some changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 
-const {followUser,unFollowUser} = require("../controllers/userController");
+const { followUser, unFollowUser } = require("../controllers/userController");
 
-router.post('/:id/follow',authMiddleware,followUser);
-router.post('/:id/unfollow',authMiddleware,unFollowUser);
+router.post('/:id/follow', authMiddleware, followUser);
+router.post('/:id/unfollow', authMiddleware, unFollowUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/auth', authRoutes);
-app.use('/api/users',userRoutes);
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
