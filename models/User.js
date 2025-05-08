cff0c71f03e34099df00271b1b7f92739b0acf40
Piyush <piyushsainiyt868@gmail.com>
2025-05-08 10:20:30 +0530

    some schema changes and bug fixes

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username: String,
-    eamil: String,
+    email: String,
     password: String,
     followers: [{
         type: mongoose.Schema.Types.ObjectId,
